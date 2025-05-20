ee3bea1f919a77c8fff8261a0bfa2496481e1491
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-20 14:32:08 -0300

    ✨feat: include Article entity in data source configuration

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import User from "../app/entity/User"
+import Article from "../app/entity/Article"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
@@ -11,7 +12,7 @@ export const AppDataSource = new DataSource({
     database: "test",
     synchronize: true,
     logging: false,
-    entities: [User],
+    entities: [User, Article],
     migrations: [],
     subscribers: [],
 })
