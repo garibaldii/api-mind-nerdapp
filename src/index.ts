99a9764076283fbcb0d943e51529b59e6e41a9af
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-20 14:28:26 -0300

    ✨feat:: initialize Express server with CORS and database connection

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+
+
+import { AppDataSource } from "./database/data-source"
+import User from "./app/entity/User"
+import routers from './app/routes/routes';
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(routers);
+
+AppDataSource.initialize().then(async () => {
+
+    console.log("Database OK")
+
+    app.listen(3333, () => {
+        console.log("Server started on port 3333")
+    })
+
+}).catch(error => console.log(error))
