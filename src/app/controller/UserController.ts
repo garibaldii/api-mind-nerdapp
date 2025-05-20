74ae9f4a3559d49902a383651dba79250142e164
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-20 17:07:53 -0300

    ✨feat: enhance UserController and UserRepository with login functionality and improve type definitions

--- src/app/controller/UserController.ts
+++ src/app/controller/UserController.ts
@@ -1,6 +1,6 @@
 import { Request, Response, Router } from 'express';
 import UserRepository from '../repositories/UserRepository';
-import { signUp } from '../../service/AuthService';
+import { login, signUp } from '../../service/AuthService';
 
 const userRouter = Router();
 
@@ -9,21 +9,32 @@ userRouter.get("/", async (req: Request, res: Response) => {
         const users = await UserRepository.getUsers();
         res.status(200).json(users)
     } catch (error) {
-        console.error(error)
+        console.error(error)//
     }
 
 })
 
-userRouter.post("/", async (req: Request, res: Response) => {
+userRouter.post("/signUp", async (req, res, next) => {
 
     try {
         const { name, email, password } = req.body
         const user = await signUp(name, email, password)
         res.status(201).json({ message: "Usuário cadastrado com sucesso", user })
-    } catch (error) {
-        console.error(error)
+    } catch (error: any) {
+        next(error)
     }
 
+})
+
+userRouter.post("/login", async (req, res, next) => {
+    try {
+        const { email, password } = req.body
+        const token = await login(email, password)
+        res.status(201).send({ token })
+
+    } catch (error) {
+        next(error)
+    }
 })
 
 export default userRouter;
--- src/app/repositories/UserRepository.ts
+++ src/app/repositories/UserRepository.ts
@@ -5,18 +5,24 @@ import { IUser } from "../interfaces/IUser";
 
 const userRepository = AppDataSource.getRepository(User);
 
-const getUsers = (): Promise<IUser[]> => {
+const getUsers = (): Promise<User[]> => {
     return userRepository.find();
 }
 
- const postUser = async (user: User): Promise<IUser> => {
+const postUser = async (user: IUser): Promise<User> => {
     return userRepository.save(user);
 }
 
- const getUserByEmail = async (email: string): Promise<IUser> => {
+const getUserByEmail = async (email: string): Promise<User> => {
     return userRepository.findOne({
-        where: {email},
+        where: { email },
     })
 }
 
-export default { getUsers, postUser, getUserByEmail };
+const getUserById = async (id: number): Promise<User> => {
+    return userRepository.findOne({
+        where: { id }
+    })
+}
+
+export default { getUsers, postUser, getUserByEmail, getUserById };
