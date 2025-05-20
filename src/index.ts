0c213d676008203640d1f02f02e0e69fd727ab87
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-20 17:09:38 -0300

    ✨ enhance AuthService with JWT secret logging and add validation for required fields in signUp

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
 import express from 'express'
 import cors from 'cors'
-
+import dotenv from 'dotenv'
 
 import { AppDataSource } from "./database/data-source"
-import User from "./app/entity/User"
-import routers from './app/routes/routes';
+import routers from './routes/routes';
+
+dotenv.config()
 
 const app = express();
+const port = process.env.PORT || 3334
 
 app.use(cors());
-
 app.use(express.json());
-
 app.use(routers);
 
 AppDataSource.initialize().then(async () => {
 
-    console.log("Database OK")
+    console.log("Database OK ✅")
 
-    app.listen(3333, () => {
-        console.log("Server started on port 3333")
+    app.listen(port, () => {
+        console.log(`Server started on port ${port} 🚪`)
     })
 
 }).catch(error => console.log(error))
