31521513a48528d47c9a4f8a29927e0e7083d1d9
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-20 20:20:57 -0300

    ✨ feat: enhance ArticleController with service methods and image upload handling

--- src/app/controller/ArticleController.ts
+++ src/app/controller/ArticleController.ts
@@ -1,40 +1,62 @@
-import { Request, Response, Router } from 'express';
-import { getArticles, saveArticle } from '../../service/ArticleService';
+import { Request, Response, Router } from "express";
+import {
+    deleteArticleService,
+  getArticlesService,
+  saveArticleService,
+  updateArticleService,
+} from "../service/ArticleService";
+
+import { upload } from "../../middleware/upload";
 
 const articleRouter = Router();
 
 //READ
 articleRouter.get("/", async (req, res, next) => {
-    try {
-        const articles = await getArticles()
-        res.status(200).send({ articles })
-    } catch (error) {
-        next(error)
-    }
-})
+  try {
+    const articles = await getArticlesService();
+    res.status(200).send({ articles });
+  } catch (error) {
+    next(error);
+  }
+});
 
 //CREATE
-articleRouter.post("/", async (req, res, next) => {
-    try {
-        const {
-            title,
-            content,
-            authorId,
-            image
-        } = req.body;
+articleRouter.post("/", upload.single("image"), async (req, res, next) => {
+  try {
+    const { title, content, authorId } = req.body;
+    const image = req.file?.buffer
 
-        const article = await saveArticle(title, content, authorId, image)
-        res.status(201).send({ article })
-    } catch (error) {
-        next(error)
-    }
-})
+    const article = await saveArticleService(title, content, Number(authorId), image);
+    res.status(201).send({ article });
+  } catch (error) {
+    next(error);
+  }
+});
 
 //UPDATE
-articleRouter.put("/:id", async (req, res, next) => {
+articleRouter.put("/:id", upload.single("image"),async (req, res, next) => {
+  try {
+    const id = Number(req.params.id);
+    const { title, content, authorId } = req.body;
+    const image = req.file?.buffer;
+
+    const data = {title, content, authorId, image}
+
+    const article = await updateArticleService(id, data);
+
+    res.status(200).send({ article });
+  } catch (error) {
+    next(error);
+  }
+});
+
+//DELETE
+articleRouter.delete("/:id", async (req, res, next) => {
     try {
         const id = Number(req.params.id)
-        const { title, content, image } = req.body
+        const response = await deleteArticleService(id)
+
+        res.status(200).send({message: `Artigo excluído com sucesso`, response})
     } catch (error) {
         next(error)
     }
