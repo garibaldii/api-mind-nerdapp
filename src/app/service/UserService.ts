5ccd20699db4cc11013fc3bcedd2d48f91de05be
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-22 15:40:59 -0300

    ♻️ refactor: update putUser function to handle partial user data updates

--- src/app/service/UserService.ts
+++ src/app/service/UserService.ts
@@ -1,8 +1,8 @@
 import { HttpError } from "../../utils/HttpError"
+import User from "../entity/User"
 import UserRepository from "../repositories/UserRepository"
 
 export const putUser = async (id: number, data: any) => {
-
     const user = await UserRepository.getUserById(id)
 
     if (!user) {
@@ -19,7 +19,12 @@ export const putUser = async (id: number, data: any) => {
         )
     }
 
-    return await UserRepository.putUser(user, data)
+    const updatedData: Partial<User> = {}
+
+    if(data.name !== undefined) updatedData.name = data.name
+    if(data.photo !== undefined) updatedData.photo = data.photo
+
+    return await UserRepository.putUser(user, updatedData)
 }
 
 export const getUserByIdService = async (id: number) => {
