1ce8b29412260ee3f665dd006be98e7f451a9c2a
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-22 20:30:22 -0300

    ✨ feat: implement like and unlike functionality in UserService and UserRepository

--- src/app/service/UserService.ts
+++ src/app/service/UserService.ts
@@ -1,56 +1,77 @@
-import { HttpError } from "../../utils/HttpError"
-import User from "../entity/User"
-import ArticleRepository from "../repositories/ArticleRepository"
-import UserRepository from "../repositories/UserRepository"
+import { HttpError } from "../../utils/HttpError";
+import User from "../entity/User";
+import ArticleRepository from "../repositories/ArticleRepository";
+import UserRepository from "../repositories/UserRepository";
 
 export const putUser = async (id: number, data: any) => {
-    const user = await UserRepository.getUserById(id)
+  const user = await UserRepository.getUserById(id);
 
-    if (!user) {
-        throw new HttpError(
-            `Usuário não existe`,
-            404
-        )
-    }
+  if (!user) {
+    throw new HttpError(`Usuário não existe`, 404);
+  }
 
-    if (!data) {
-        throw new HttpError(
-            `Não existem dados novos a serem atualizados`,
-            404
-        )
-    }
+  if (!data) {
+    throw new HttpError(`Não existem dados novos a serem atualizados`, 404);
+  }
 
-    const updatedData: Partial<User> = {}
+  const updatedData: Partial<User> = {};
 
-    if (data.name !== undefined) updatedData.name = data.name
-    if (data.photo !== undefined) updatedData.photo = data.photo
+  if (data.name !== undefined) updatedData.name = data.name;
+  if (data.photo !== undefined) updatedData.photo = data.photo;
 
-    return await UserRepository.putUser(user, updatedData)
-}
+  return await UserRepository.putUser(user, updatedData);
+};
 
 export const getUserByIdService = async (id: number) => {
-    return await UserRepository.getUserById(id)
-}
+  return await UserRepository.getUserById(id);
+};
 
 export const likeArticleService = async (userId: number, articleId: number) => {
-    const user = await UserRepository.getUserById(userId)
-    const article = await ArticleRepository.getArticleById(articleId)
+  const user = await UserRepository.getUserById(userId);
+  const article = await ArticleRepository.getArticleById(articleId);
 
-    if (!user || !article) { throw new HttpError(`Usuário ou Artigo não existe`, 404) }
+  if (!user || !article) {
+    throw new HttpError(`Usuário ou Artigo não existe`, 404);
+  }
 
-    if (!user.likedArticles) { user.likedArticles = [] }
+  user.likedArticles = user.articles ?? [];
 
-    const alreadyLiked = user.likedArticles.some((article) => article.id === articleId)
-    if (alreadyLiked) { throw new HttpError(`Usuário já curtiu este artigo`, 400) }
+  const liked = user.likedArticles.some(
+    (article) => Number(article.id) === Number(articleId)
+  );
 
-    //atualiza a contagem de like do artigo
-    ArticleRepository.incrementLike(articleId)
+  console.log(liked);
+  console.log(user.likedArticles);
+  if (liked) {
+    throw new HttpError(`Usuário já curtiu este artigo`, 400);
+  }
 
-    //joga o artigo para a lista de artigos curtidos por aquele usuário.
-    user.likedArticles.push(article)
+  //atualiza a contagem de like do artigo
+  ArticleRepository.incrementLike(articleId);
 
-    //neste caso, o typeORM diferencia um objeto através da presenca ou nao de seu ID,
-    //logo, iremos salvar a atualizacao de likedArticles deste objeto. 
-    return UserRepository.postUser(user)
+  //joga o artigo para a lista de artigos curtidos por aquele usuário.
+  return await UserRepository.likeArticle(userId, articleId);
 
+  
+};
+
+export const unlikeArticleService = async (userId: number, articleId: number) => {
+    const user = await UserRepository.getUserById(userId);
+  const article = await ArticleRepository.getArticleById(articleId);
+
+  if (!user || !article) {
+    throw new HttpError(`Usuário ou Artigo não existe`, 404);
+  }
+
+    const liked = user.likedArticles.some(
+    (article) => Number(article.id) === Number(articleId)
+  );
+
+  if(!liked) {
+    throw new HttpError(`Usuário não curtiu este artigo, nao é possível descurtir`, 400)
+  }
+
+  ArticleRepository.decrementLike(articleId)
+
+  return await UserRepository.unlikeArticle(userId, articleId)
 }
