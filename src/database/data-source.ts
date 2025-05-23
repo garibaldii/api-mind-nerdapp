4e295cd430a505f210956d4343b65ef061c5acc8
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-22 21:19:59 -0300

    ♻️ refactor: add environment variables for database configuration in data-source.ts

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,18 +1,21 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import User from "../app/entity/User"
-import Article from "../app/entity/Article"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import User from "../app/entity/User";
+import Article from "../app/entity/Article";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "admin",
-    database: "db-nerdapp",
-    synchronize: true,
-    logging: false,
-    entities: [User, Article],
-    migrations: [],
-    subscribers: [],
-})
+  type: "mysql",
+  host:process.env.MYSQLHOST,
+  port: Number(process.env.MYSQLPORT),
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  synchronize: true,
+  logging: false,
+  entities: [User, Article],
+  migrations: [],
+  subscribers: [],
+});
